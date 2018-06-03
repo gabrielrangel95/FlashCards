791e7bfd9494edf888ad717ae2014d2ffaf1f52a
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-06-03 00:34:30 -0300

    feat: main screen create

---
+++ src/screens/Main/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Title,
+} from './MainStyle';
+
+class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <Title>FlashCards</Title>
+      </Container>
+    );
+  }
+}
+
+export { Main };
+
---
+++ src/screens/Main/MainStyle.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1
+    flex-direction: column;
+`;
+
+const Title = styled.Text`
+    font-size: 18;
+    font-weight: bold;
+    color: black;
+`;
+
+export {
+    Container,
+    Title,
+}

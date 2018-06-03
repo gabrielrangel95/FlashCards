14d0a3c939c74e3eaa4c590da0230c50d75a3560
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-06-03 02:43:46 -0300

    feat: redux implement

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { View, StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 import { Navigator } from './route';
+import store from './redux/store';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <StatusBar
-          backgroundColor="transparent"
-          barStyle="light-content"
-        />
-        <Navigator />
-      </View>
+      <Provider store={store}>
+        <View style={{flex: 1}}>
+          <StatusBar
+            backgroundColor="transparent"
+            barStyle="light-content"
+          />
+          <Navigator />
+        </View>
+      </Provider>
     );
   }
 }

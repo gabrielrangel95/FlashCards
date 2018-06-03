80bc697a804a28a1721170a2e1bb829b65ca0486
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-06-03 00:39:29 -0300

    feat: status bar color implement

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { View, StatusBar } from 'react-native';
 import { Navigator } from './route';
 
 export default class App extends Component {
   render() {
     return (
-      <Navigator />
+      <View>
+        <StatusBar
+          backgroundColor="transparent"
+          barStyle="light-content"
+        />
+        <Navigator />
+      </View>
     );
   }
 }

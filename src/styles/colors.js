fb43f2063b01199e8be53aa8862ce3378dad9b6e
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-06-03 00:39:17 -0300

    feat: colors file created

---
+++ src/styles/colors.js
@@ -0,0 +1,19 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#9B9B9B',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  primary: '#7919E4',
+  secundary: '#FF4895',
+  tertiary: '#F6D704',
+  success: '#9DCA83',
+  danger: '#E37A7A',
+
+  transparent: 'transparent',
+  whiteTransparent: 'rgba(255, 255, 255, 0.2)',
+  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+};

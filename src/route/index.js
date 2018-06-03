37856362064c5e95008b90b654989c448d2a1a3b
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-06-03 00:39:41 -0300

    feat: navigation options implement

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { Main } from '../screens';
+import { colors } from '../styles';
 
 const Navigator = createStackNavigator(
   {
     Main: {
       screen: Main
+    }
+  },
+  {
+    navigationOptions:{
+      headerStyle: {
+        backgroundColor: colors.black,
+      },
+      headerTintColor: colors.white,
+      headerBackTitle: null,
     }
   }
 )
